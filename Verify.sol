00531aed4692c4629f822a62bd6bb0cd12716818
Wright <calebogundiya@gmail.com>
2022-03-23 11:13:54 +0100

    Fix order of returned v,r,s

--- Verify.sol
+++ Verify.sol
@@ -1,17 +1,20 @@
 pragma solidity ^0.5.0;
 
 contract Verify {
-    
     address importantAddress;
 
-    constructor (address _importantAddress) public {
+    constructor(address _importantAddress) public {
         importantAddress = _importantAddress;
     }
 
-    function splitSignature(bytes memory sig) 
-        public 
-        pure 
-        returns(uint8, bytes32, bytes32)
+    function splitSignature(bytes memory sig)
+        public
+        pure
+        returns (
+            bytes32,
+            bytes32,
+            uint8
+        )
     {
         require(sig.length == 65);
         bytes32 r;
@@ -19,16 +22,15 @@ contract Verify {
         uint8 v;
 
         assembly {
-            // first 32 bytes, after the length prefix
+            // next 32 bytes, after the length prefix
             r := mload(add(sig, 32))
-            // second 32 bytes
+            // next 32 bytes after r
             s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
+            // final byte (first byte of the next 32 bytes) - last byte in the signature, mload picks 32 bytes at a time, so we need to pick just the first 2
             v := byte(0, mload(add(sig, 96)))
         }
 
-        return (v, r, s);
-
+        return (r, s, v);
     }
 
     function recoverSigner(bytes32 message, bytes memory sig)
@@ -39,18 +41,15 @@ contract Verify {
         uint8 v;
         bytes32 r;
         bytes32 s;
-        (v,r,s) = splitSignature(sig);
-        return ecrecover(message, v,r,s);
+        (v, r, s) = splitSignature(sig);
+        return ecrecover(message, v, r, s);
     }
 
     function isValidData(
-        uint256 _number, 
-        string memory _word, 
+        uint256 _number,
+        string memory _word,
         bytes memory sig
-    ) 
-        public
-        returns (bool)
-    {
+    ) public returns (bool) {
         bytes32 message = keccak256(abi.encodePacked(_number, _word));
         return (recoverSigner(message, sig) == importantAddress);
     }
